1a68b6f9a4ecd6397898ae8e29b897e565ed3c5d
ericgarcia-e <eagarcia@unitedvirtualities.com>
2019-12-03 10:41:58 -0800

    step-2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
- 
+app.get('/users', (req, res) => {
+res.json([]);
+})
 app.listen(3000, ()=>{
     console.log("listen the port 3000")
 });
