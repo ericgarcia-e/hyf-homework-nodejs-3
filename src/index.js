935e36ea88b60652417f020540ba7b575b2a2654
ericgarcia-e <eagarcia@unitedvirtualities.com>
2019-12-03 12:09:00 -0800

    step-3

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 app.get('/users', (req, res) => {
-res.json([users]);
+res.json(users);
 })
 app.post('/users', (req, res) => {
  users.push({id:0})
-  res.json([users]);
+  res.json(users);
   })
-app.get('/user/:id ', (req, res) => {
-
-  res.json([{id:0}]);
-  })
+  app.get('/user/:id', (req, res) => {
+    const user = users.find(user => user.id == req.params.id);
+    res.json(user);
+});
 app.listen(3000, ()=>{
     console.log("listen the port 3000")
 });
