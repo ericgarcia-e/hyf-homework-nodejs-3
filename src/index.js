b216c45dd8d3a0ce61fb3bdbcea5bd67ac2a3ca1
ericgarcia-e <eagarcia@unitedvirtualities.com>
2019-12-03 15:14:35 -0800

    step-4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const aplication = express()
 let users = [];
+let user=[];
  
 aplication.get('/', function (req, res) {
   res.send('Hello World!')
@@ -21,7 +22,8 @@ aplication.post('/user', (req, res) => {
     res.json(user);
 });
 aplication.delete('/user/:id', (req, res) => {
-  const user = users.find(user => user.id == req.params.id);
+  //const user = [];
+  //users.find(user => user.id == req.params.id);
   if(user==true){
 res.status(200).json(user);
     
